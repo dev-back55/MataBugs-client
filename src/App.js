f2025da5dae9d27de934db12e740a4c53d1f8e0d
Federico <romerof14@gmail.com>
2022-08-27 18:06:45 -0300

    FEAT/SEARCHVIEW: Agregada vista de busqueda de usuarios La misma incluye: Selects de ordenamientos, controles de paginacion y listado de cartas.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import React from 'react';
-import Cards from './components/Cards/Cards';
-import CustomSelect from './components/CustomSelect/CustomSelect';
-import PaginationControllers from './components/PaginationControllers/PaginationControllers';
+import SearchView from './views/SearchView/SearchView';
 import './App.css';
-
-import { listOfUsers } from './lib/userModel';
 
 function App() {
 
-  const [ value, setValue ] = React.useState("ranking");
-  const [ order, setOrder ] = React.useState("ascending");
-
   return (
     <div className="App">
-      <CustomSelect
-        valueSelected = {value}
-        values = {["ranking", "nickname"]}
-        handleValue = {(newValue) => setValue(newValue)}
-      />
-      <CustomSelect
-        valueSelected = {order}
-        values = {["ascending", "descending"]}
-        handleValue = {(newValue) => setOrder(newValue)}
-      />
-      <PaginationControllers />
-      <br></br>
-      <Cards
-        users = {listOfUsers}
-      />
+      <SearchView />
     </div>
   );
 }
--- src/components/PaginationControllers/PaginationControllers.jsx
+++ src/components/PaginationControllers/PaginationControllers.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import CustomSelect from '../CustomSelect/CustomSelect';
 import PaginationButton from '../PaginationButton/PaginationButton';
-
 import s from './PaginationControllers.module.css';
 
 export default function PaginationControllers() {
+
+  const [ value, setValue ] = React.useState("ranking");
+  const [ order, setOrder ] = React.useState("ascending");
+
   return (
     <div className = {s.container}>
+      <CustomSelect
+        valueSelected = {value}
+        values = {["ranking", "nickname"]}
+        handleValue = {(newValue) => setValue(newValue)}
+      />
+      <CustomSelect
+        valueSelected = {order}
+        values = {["ascending", "descending"]}
+        handleValue = {(newValue) => setOrder(newValue)}
+      />
       <PaginationButton
         disabled = {false}
         handler = {() => console.log("Ok")}
