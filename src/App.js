06965eb41171365e89af03db18b16b1ed819a22b
gpitrella <gabriel98_@hotmail.com>
2022-08-28 19:43:48 -0300

    update branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import PlayerDetail from './PlayerDetail/PlayerDetail';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Vamos MATABUGS</h1>
+      <PlayerDetail />
     </div>
   );
 }
