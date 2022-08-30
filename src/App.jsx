4d3fcd9416dbccc759be0a1f375d3cd0bff75100
Federico <romerof14@gmail.com>
2022-08-30 09:58:41 -0300

    FEAT/NAVBAR: Merge con develop.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import NavBar from './components/NavBar/NavBar';
+import LogInLogUp from './components/LogInLogUp/LogInLogUp';
 import SearchView from './views/SearchView/SearchView';
 import CardDetails from './components/CardDetails/CardDetails';
 import HallOfFameView from './views/HallOfFameView/HallOfFameView';
 import './App.css';
-import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
+
+   const { player } = useSelector((state) => state.player);
+
+  useEffect(() => {
+      // Envia a Home cuando esta logeado
+  }, [player])
 
   return (
     <div className="App">
       <NavBar />
       <Routes>
         <Route exact path = '/' element = { <SearchView /> } />
+        <Route exact path = '/login' element = { !player?.player ? <LogInLogUp /> : <Navigate to="/"/> } />
         <Route exact path = '/player/:id' element = { <CardDetails /> } />
         <Route exact path = '/hallOfFame' element = { <HallOfFameView /> } />
         <Route path = "*" element = { <Navigate to = "/" replace /> } />
