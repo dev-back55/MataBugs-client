03649753337243f36b31ff9aa1fecd3703eb35a4
gpitrella <gabriel98_@hotmail.com>
2022-08-30 16:20:29 -0300

    update sidebar

--- src/views/SearchView/SearchView.jsx
+++ src/views/SearchView/SearchView.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Cards from '../../components/Cards/Cards';
 import PaginationControllers from '../../components/PaginationControllers/PaginationControllers';
+import Sidebar from '../../components/Sidebar/Sidebar';
 import { listOfUsers } from '../../lib/userModel';
 
 import s from './SearchView.module.css';
@@ -8,11 +9,12 @@ import s from './SearchView.module.css';
 export default function SearchView() {
 
   return (
-    <div className = {s.container}>
-      <PaginationControllers />
-      <Cards
-        users = {listOfUsers}
-      />
-    </div>
+      <div className = {s.container}>
+          <PaginationControllers />
+          <Cards
+            users = {listOfUsers}
+            />
+      </div>
+     
   );
 }
